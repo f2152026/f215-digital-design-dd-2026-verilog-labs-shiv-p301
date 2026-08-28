b67f60e138eef42399a404f4e575473ebbf1a07b
shiv-p301 <f20250773@pilani.bits-pilani.ac.in>
2026-08-28 21:07:11 +0000

    lab01 task3

--- labs/lab01/task3/FA_Gate.v
+++ labs/lab01/task3/FA_Gate.v
@@ -26,10 +26,10 @@ module FA_Gate(
 );
   wire ps, pc1, pc2;
 
-  xor #(3,4) (ps,  a,   b);
-  and #(3,4) (pc1, a,   b);
-  xor #(3,4) (sum, cin, ps);
-  and #(3,4) (pc2, cin, ps);
-  or #(3,4)  (cout, pc1, pc2);
+  xor #(2) (ps,  a,   b);
+  and #(2) (pc1, a,   b);
+  xor #(2) (sum, cin, ps);
+  and #(2) (pc2, cin, ps);
+  or #(2)  (cout, pc1, pc2);
 
 endmodule
